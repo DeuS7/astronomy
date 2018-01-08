bdd81def298b97786f9af155a345702a9c0575fa
DeuS7 <never-99@yandex.com>
2018-01-08 18:25:00 +0500

    Added slide show

--- app/libs/pepperMint/pepperSettings.js
+++ app/libs/pepperMint/pepperSettings.js
@@ -20,25 +20,3 @@ for (var i = 0;i<mints.length;i++) {
     onSetup: undefined
 }));
 }
-
-var slideShows = document.getElementsByClassName("peppermintSlideShow");
-
-for (var i = 0;i<mints.length;i++) {
-    Peppermint(slideShows[i], {
-    speed: 300,
-    touchSpeed: 300,
-    slideshow: true,
-    slideshowInterval: 4000,
-    stopSlideshowAfterInteraction: true,
-    startSlide: 0,
-    mouseDrag: false,
-    disableIfOneSlide: true,
-    cssPrefix: 'peppermint-',
-    dots: true,
-    dotsPrepend: false,
-    dotsContainer: undefined,  
-    slidesContainer: undefined,
-    onSlideChange: undefined,
-    onSetup: undefined
-})
-}
---
+++ app/swiperSettings.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  speed: 1000,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  },
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1
+  }
+});
